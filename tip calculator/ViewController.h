263ed25237944620a1195a88285e582d20c312fe
Gustavo Coutinho <guscoutinho@fb.com>
2018-06-26 17:51:06 -0700

    teeps calculator

--- tip calculator/ViewController.h
+++ tip calculator/ViewController.h
@@ -4,7 +4,7 @@
 //
 //  Created by Gustavo Coutinho on 6/26/18.
 //  Copyright © 2018 Gustavo Coutinho. All rights reserved.
-//
+///Users/guscoutinho/Desktop/tip_calculator/tip calculator/ViewController.m
 
 #import <UIKit/UIKit.h>
 
--- tip calculator/ViewController.m
+++ tip calculator/ViewController.m
@@ -1,6 +1,6 @@
 //
 //  ViewController.m
-//  tip calculator
+//  Teeeps
 //
 //  Created by Gustavo Coutinho on 6/26/18.
 //  Copyright © 2018 Gustavo Coutinho. All rights reserved.
@@ -18,13 +18,25 @@
 
 @property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
 
+@property (weak, nonatomic) IBOutlet UILabel *plusLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *equalLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *sharingLabel;
+
+@property (weak, nonatomic) IBOutlet UISwitch *switchLabel;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.tipLabel.alpha = 0;
+    self.tipControl.alpha = 0;
+    self.totalLabel.alpha = 0;
+    self.plusLabel.alpha = 0;
+    self.equalLabel.alpha = 0;
 }
 
 
@@ -32,6 +44,7 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
 
 - (IBAction)onTap:(id)sender {
     [self.view endEditing:YES];
@@ -51,35 +64,60 @@
     self.tipLabel.text = [NSString stringWithFormat:@"$%.2f", tip];
     self.totalLabel.text = [NSString stringWithFormat:@"$%.2f", total];
 }
+
+
 - (IBAction)onEditingBegin:(id)sender {
-    
+
     [UIView animateWithDuration:0.2 animations:^{
-self.billField.frame = CGRectMake(self.billField.frame.origin.x, self.billField.frame.origin.y + 30, self.billField.frame.size.width, self.billField.frame.size.height);
-        
-        
+self.billField.frame = CGRectMake(self.billField.frame.origin.x, self.billField.frame.origin.y - 80, self.billField.frame.size.width, self.billField.frame.size.height);
+
     }];
     
-    [UIView animateWithDuration: 1.0 animations:^{
-       self.tipLabel.alpha = 0;
+    CGRect tipFrame = self.tipLabel.frame;
+    CGRect tipControlFrame = self.tipControl.frame;
+    CGRect totalFrame = self.totalLabel.frame;
+    CGRect plusFrame = self.plusLabel.frame;
+    CGRect equalFrame = self.equalLabel.frame;
+//    CGRect sharingLabel = self.sharingLabel.frame;
+//    CGRect switchLabel = self.switchLabel.frame;
+
+    tipFrame.origin.y -= 80;
+    tipControlFrame.origin.y -= 80;
+    totalFrame.origin.y -= 80;
+    plusFrame.origin.y -= 80;
+    equalFrame.origin.y -= 80;
+//    sharingLabel.origin.y -= 80;
+//    switchLabel.origin.y -= 80;
+
+    [UIView animateWithDuration: 0.2 animations:^{
+        self.tipLabel.frame = tipFrame;
+        self.tipControl.frame = tipControlFrame;
+        self.totalLabel.frame = totalFrame;
+        self.plusLabel.frame = plusFrame;
+        self.equalLabel.frame = equalFrame;
+//        self.sharingLabel.frame = sharingLabel;
+//        self.switchLabell.frame = switchLabel;
+        
+        self.tipLabel.alpha = 1;
+        self.tipControl.alpha = 1;
+        self.totalLabel.alpha = 1;
+        self.plusLabel.alpha = 1;
+        self.equalLabel.alpha = 1;
     }];
-    
+
 }
 
 
 - (IBAction)onEditingEnd:(id)sender {
-    CGRect newFrame = self.billField.frame;
-    newFrame.origin.y -= 30;
-    
-    self.billField.frame = newFrame;
-    
+
     [UIView animateWithDuration:0.2 animations:^{
-        self.billField.frame = newFrame;
         self.tipLabel.alpha = 1;
+        self.tipControl.alpha = 1;
+        self.totalLabel.alpha = 1;
+        self.plusLabel.alpha = 1;
+        self.equalLabel.alpha = 1;
     }];
-    
-    [UIView animateWithDuration: 1.0 animations:^{
-        self.tipLabel.alpha = 1;
-    }];
+
 }
 
 @end
